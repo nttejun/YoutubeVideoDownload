79223ca4c83201bc2b95888072de1eaf46f28e89
Wonjun Jeong <nttejun@gmail.com>
2017-07-21 18:39:15 +0900

    decode file download

--- app.js
+++ app.js
@@ -1,46 +1,35 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const main = require('./router/main');
 
-var app = express();
+app.listen(3000, function(){
+    console.log("connect");
+});
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/main', main);
 
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/', function (req, res) {
+    console.log('/ get');
+    res.sendFile(__dirname + '/public/testHtml.html');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.post('/videoSearch', function (req, res) {
+    console.log('/videoSearch');
+    console.log(req.body.videoName);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    var searchSplit = req.body.videoName.split('https://youtu.be/');
+    console.log(searchSplit[1]);
+
+    var videoSearchData = 'https://www.youtube.com/get_video_info?video_id='+searchSplit[1];
+    console.log(videoSearchData);
+    res.send("<a href='"+videoSearchData+"'>동영상 다운로드</a>")
+    res.sendFile(__dirname + '/public/testHtml.html');
+
 });
-
-module.exports = app;
